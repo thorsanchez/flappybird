eb38a64ae5a40501b28b975bcf71c27b9a5ee233
thorsanchez <ths281@hi.is>
2026-02-05 14:32:43 +0000

    base og pipe class

--- flappybird.py
+++ flappybird.py
@@ -29,7 +29,7 @@ class Bird:
         self.y = y
         self.tilt = 0
         self.tick_count = 0
-        self.vel = 0
+        self.velocity = 0
         self.height = self.y
         self.img_count = 0
         self.img = self.IMGS[0]
@@ -86,20 +86,101 @@ class Bird:
     def get_mask(self):
         return pygame.mask.from_surface(self.img)
     
-def draw_window(win, bird):
+class Pipe:
+    GAP = 200
+    VEL = 5
+
+    def __init__(self, x):
+        self.x = x
+        self.height = 0
+        self.gap = 100
+        self.top = 0
+        self.bottom = 0
+        #uppniður pipe
+        self.PIPE_TOP = pygame.transform.flip(PIPE_IMAGES, False, True)
+        self.PIPE_BOTTOM = PIPE_IMAGES
+        self.passed = False
+        self.set_height()
+    def set_height(self):
+        #teikna pip a negative location
+        self.height = random.randrange(50, 450)
+        self.top = self.height - self.PIPE_TOP.get_height()
+        self.bottom = self.height + self.GAP
+    def move(self):
+        self.x -= self.VEL
+
+    def draw(self, win):
+        win.blit(self.PIPE_TOP, (self.x, self.top))
+        win.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
+
+    #nota mask til að tekka a collision á ekki transparent pixels og ekki bara box
+    def collide(self, bird):
+        #gera mask fyrir bird og pipes
+        bird_mask = bird.get_mask()
+        top_mask = pygame.mask.from_surface(self.PIPE_TOP)
+        bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
+        #reikna offset (hvað eru hlutirnir langt fra hvor öðrum?)
+        top_offset = (self.x - bird.x, self.top - round(bird.y))
+        bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
+        #er collision?
+        b_point = bird_mask.overlap(bottom_mask, bottom_offset)
+        t_point = bird_mask.overlap(top_mask, top_offset)
+        #tekka ef þessi punktar seu til?
+        if t_point or b_point:
+            return True
+        return False    
+
+class Base:
+    VEL = 5
+    WIDTH = BASE_IMAGES.get_width()
+    IMG = BASE_IMAGES
+
+    def __init__(self, y):
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.WIDTH
+
+    def move(self):
+        #færa tvær myndir
+        self.x1 -= self.VEL
+        self.x2 -= self.VEL
+
+        if self.x1 + self.WIDTH < 0:
+            self.x1 = self.x2 + self.WIDTH
+
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
+    #teikna myndirnar
+    def draw(self, win):
+        win.blit(self.IMG, (self.x1, self.y))
+        win.blit(self.IMG, (self.x2, self.y))
+
+
+def draw_window(win, bird, pipes, base):
     win.blit(BG_IMAGES, (0,0))
+
+    #teikna allar pipes
+    for pipe in pipes:
+        pipe.draw(win)
+    base.draw(win)
+
     bird.draw(win)
     pygame.display.update()
        
 def main():
-    bird = Bird(200, 200)
+    bird = Bird(230, 350)
+    base = Base(730)
+    pipes = [Pipe(700)]
     win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+    clock = pygame.time.Clock()
     run = True
     while run:
+        clock.tick(30)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        draw_window(win, bird)
+        #bird.move()
+        draw_window(win, bird, pipes, base)
     pygame.quit()
     quit()
 main()
